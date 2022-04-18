73c2b63e6ce091da73ee3ef11164c1991755de9a
chenzhibin <chenzhibin@inke.cn>
2022-04-18 08:45:52 +0800

    发布文章，频道选择功能提交

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,20 +1,98 @@
 <template>
-  <div class="publish-container">文章发布</div>
+  <div class="publish-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!--    面包屑控件 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to='/' >首页</el-breadcrumb-item>
+          <el-breadcrumb-item>发布文章</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!--  /面包屑控件  -->
+      </div>
+      <div >
+        <el-form ref="form" :model="article" label-width="80px">
+          <el-form-item label="标题">
+            <el-input v-model="article.title"></el-input>
+          </el-form-item>
+          <el-form-item label="内容">
+            <el-input type="textarea" v-model="article.content"></el-input>
+          </el-form-item>
+          <el-form-item label="封面">
+            <el-radio-group v-model="article.content">
+              <el-radio label="单图"></el-radio>
+              <el-radio label="三图"></el-radio>
+              <el-radio label="无图"></el-radio>
+              <el-radio label="自动"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="频道">
+            <el-select v-model="article.channels" placeholder="请选择频道">
+              <el-option
+                :label="channel.name"
+                :value="channel.id"
+                v-for="(channel,index) in channels"
+                :key="index"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">发布</el-button>
+            <el-button>存入草稿</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import { getArticleChannels } from '@/api/articles'
+
 export default {
   name: 'PublishIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      article: {
+        title: '',
+        content: '',
+        cover: {
+          type: 0,
+          image: []
+        },
+        channels: this.channels
+      },
+      channels: null
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    loadChannels () {
+      getArticleChannels().then(res => {
+        this.channels = res.data.data.channels
+      })
+    }
+  }
 }
 </script>
 
